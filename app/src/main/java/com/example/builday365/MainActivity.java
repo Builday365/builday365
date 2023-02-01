24056e5b62d11eefb3d3d73fb8455d79994835d2
HoyeonYu <builday365@gmail.com>
2023-02-02 00:48:37 +0900

    [Feat] Add Left Drawer Menu

--- app/src/main/java/com/example/builday365/MainActivity.java
+++ app/src/main/java/com/example/builday365/MainActivity.java
@@ -1,63 +1,31 @@
 package com.example.builday365;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tv_google_nickname;
-    private ImageView iv_google_photo;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById (R.id.toolbar);
-        setSupportActionBar (toolbar);
-
-        Intent intent = getIntent();
-        String google_nickname = intent.getStringExtra("google_nickname");
-        String google_photo = intent.getStringExtra("google_photo");
-
-        tv_google_nickname = findViewById(R.id.main_tv_google_nickname);
-        tv_google_nickname.setText(google_nickname);
-
-        iv_google_photo = findViewById(R.id.main_iv_google_photo);
-        Glide.with(this).load(google_photo).into(iv_google_photo);
-
-    }
-
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId ()) {
-            case R.id.appbar_top_ib_side_menu:
-                return true;
-            case R.id.appbar_top_ib_month_prev:
-                return true;
-            case R.id.appbar_top_ib_day_prev:
-                return true;
-            case R.id.appbar_top_tv_cur_date:
-                return true;
-            case R.id.appbar_top_ib_day_next:
-                return true;
-            case R.id.appbar_top_ib_month_next:
-                return true;
-            case R.id.appbar_top_ib_calendar:
-                return true;
-            default:
-                return super.onOptionsItemSelected (item);
-        }
+        ImageButton ibtn_side_menu = (ImageButton)findViewById(R.id.main_toolbar_ibtn_side_menu);
+        DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.main_drawer);
+        ibtn_side_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!drawerLayout.isDrawerOpen(Gravity.LEFT)){
+                    drawerLayout.openDrawer(Gravity.LEFT);
+                }
+                else {
+                    drawerLayout.closeDrawers();
+                }
+            }
+        });
     }
 }
