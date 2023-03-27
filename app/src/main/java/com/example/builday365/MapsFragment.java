3f0052c857b221f44a69a0bd0be321f83d364e73
HoyeonYu <builday365@gmail.com>
2023-03-28 02:00:58 +0900

    [Fix] Separate Combined Java Activity

--- app/src/main/java/com/example/builday365/MapsFragment.java
+++ app/src/main/java/com/example/builday365/MapsFragment.java
@@ -3,7 +3,6 @@ package com.example.builday365;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.app.Activity;
